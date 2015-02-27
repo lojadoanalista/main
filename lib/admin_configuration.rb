a5fa96d9382a36940a6962ccdbbd842344d21ec5
gabrielbernal <gabrielbernalbarbosa@gmail.com>
2015-02-26 23:48:49 -0300

    added option 4 set logical number

--- lib/admin_configuration.rb
+++ lib/admin_configuration.rb
@@ -7,6 +7,7 @@ class AdminConfiguration
     Device::Display.print(" 1 - Wi-fi Config", 1, 0)
     Device::Display.print(" 2 - Update Apps", 2, 0)
     Device::Display.print(" 3 - Host", 3, 0)
+    Device::Display.print(" 4 - Logical Number", 4, 0)
     key = getc
     if key == "1"
       Cloudwalk.communication
@@ -23,6 +24,9 @@ class AdminConfiguration
       elsif getc == "2"
         Device::Setting.host = "switch-staging.cloudwalk.io"
       end
+    elsif key == "4"
+      Device::Display.clear
+      Cloudwalk.logical_number
     end
   end
 end
