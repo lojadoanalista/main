ac3107a7cd560fd2b83590e9967f17e442b12d4b
gabrielbernal <gabrielbernalbarbosa@gmail.com>
2015-02-27 06:02:07 -0300

    add set clock option

--- lib/admin_configuration.rb
+++ lib/admin_configuration.rb
@@ -8,6 +8,7 @@ class AdminConfiguration
     Device::Display.print(" 2 - Update Apps", 2, 0)
     Device::Display.print(" 3 - Host", 3, 0)
     Device::Display.print(" 4 - Logical Number", 4, 0)
+    Device::Display.print(" 5 - Set Clock", 5, 0)
     key = getc
     if key == "1"
       Cloudwalk.communication
@@ -20,13 +21,31 @@ class AdminConfiguration
       Device::Display.print(" 2 - Staging", 2, 3)
 
       if getc == "1"
-        Device::Setting.host = "switch.cloudwalk.io"
+        Device::Setting.environment = "production"
       elsif getc == "2"
-        Device::Setting.host = "switch-staging.cloudwalk.io"
+        Device::Setting.environment = "staging"
       end
+
+      Device::System.restart
     elsif key == "4"
       Device::Display.clear
       Cloudwalk.logical_number
+    elsif key == "5"
+      Device::Display.clear
+      Device::Display.print("Input the year:",0,0)
+      year = Device::IO.get_format(1, 4, option  = {:mode => :numbers})
+      Device::Display.print("Input the month:",1,0)
+      month = Device::IO.get_format(1, 2, option  = {:mode => :numbers})
+      Device::Display.print("Input the day:",2,0)
+      day = Device::IO.get_format(1, 2, option  = {:mode => :numbers})
+      Device::Display.print("Input the hour:",3,0)
+      h = Device::IO.get_format(1, 2, option  = {:mode => :numbers})
+      Device::Display.print("Input the minute:",4,0)
+      m = Device::IO.get_format(1, 2, option  = {:mode => :numbers})
+      Device::Display.print("Input the second:",5,0)
+      s = Device::IO.get_format(1, 2, option  = {:mode => :numbers})
+      time = Time.new(year,month,day,h,m,s)
+      time.hwclock
     end
   end
 end
